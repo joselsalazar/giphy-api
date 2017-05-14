43a33cdabd2ff66c64127a95fa628dfd3dde17b5
Jose Salazar <jose.salazar.home@gmail.com>
2017-05-14 10:01:00 -0700

    Almost Final Changes JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,41 @@
 // Create Base Array
 var animals = [
-	"birds", "fish", "tiger", "lion", "cat", "dog", "wolf", "pug"
+	"birds", "fish", "tiger", "lion", "cat", "dog", "wolf", "pug", "shaq"
 ];
 
-// Generate Buttons From Array
-
-for (var i = 0; i < animals.length; i++) {
-	var a = $("<button>");
-	a.addClass("animals");
-	a.attr("value", animals[i]);
-	a.html(animals[i]);
-	$('.buttons').append(a);
-};
-
-// Pull AJAX Info From Giphy
+// AJAX Variables
 	var APIKey = "dc6zaTOxFJmzC";
 	var animalSelected = "";
 
+// Generate Buttons From Array
+function renderGifs() {
+	$('.buttons').empty();
+	for (var i = 0; i < animals.length; i++) {
+		var a = $("<button>");
+		a.addClass("animals");
+		a.attr("value", animals[i]);
+		a.html(animals[i]);
+		$('.buttons').append(a);
+	};
+};
+
+renderGifs();
+assignGifs();
+
+// Add Functionality to Push to Array
+$("#add-gif").click(function(event) {
+  // event.preventDefault() prevents the form from trying to submit itself.
+  event.preventDefault();
+  var newGif = $("#gif-input").val().trim();
+  animals.push(newGif);
+
+  $('#gif-input').val("");
+  renderGifs();
+  assignGifs();
+});
+
 // Bind Display AJAX to button clicks
+function assignGifs () {
 	$('.animals').click(function(){
 		animalSelected = $(this).attr("value");
 		var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animalSelected + "&api_key=" + APIKey;
@@ -28,13 +46,20 @@ for (var i = 0; i < animals.length; i++) {
 			url: queryURL,
 			method: "GET"
 		}).done(function(response) {
+			console.log(response);
 			$('.giphy').empty();
 			for (var i=0; i < 10; i++) {
-				$('.giphy').append("<img src=" + response.data[i].images.downsized.url + ">");
+				var gifDiv = $("<div>");
+				gifDiv.addClass("giphys");
+				var ratingP = $("<p>").text("Rating: " + response.data[i].rating);
+				var imageGif = $("<img>");
+				imageGif.attr("src", response.data[i].images.downsized.url);
+				gifDiv.append(ratingP);
+				gifDiv.append(imageGif);
+				$('.giphy').append(gifDiv);
 			};
 		});
 	});
+}
 
 // Add Functionality to Play/Pause Giphys
-
-// Add Functionality to Push to Array
